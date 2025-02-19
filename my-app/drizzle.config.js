187510b55480da1f311b3354a8256390895d7afe
vel <vmv2k05@gmail.com>
2025-02-19 21:17:39 +0530

    first commit

---
+++ my-app/drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:npg_XZ5TYDj7UrmW@ep-patient-mode-a5cd1e4d-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require",
+    connectionString:
+      "postgresql://neondb_owner:npg_XZ5TYDj7UrmW@ep-patient-mode-a5cd1e4d-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  },
+};
